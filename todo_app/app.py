240c1071cbf07faa44c4bf52e52c19d00979c659
JoeBawn <74307355+JoeBawn@users.noreply.github.com>
2020-11-17 16:51:36 +0000

    Step_2.2

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items, get_item, add_item, save_item
@@ -12,6 +12,11 @@ def index():
     items = get_items()
     return render_template("index.html",items=items)
 
+@app.route('/', methods=['POST'])
+def add_list():
+    newitemtitle = request.form.get('title')
+    add_item(newitemtitle)
+    return redirect(('/'))
 
 if __name__ == '__main__':
     app.run()
