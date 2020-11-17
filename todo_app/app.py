0a58b665a81c475dd6e09faa5f1c709365bf6f9f
JoeBawn <74307355+JoeBawn@users.noreply.github.com>
2020-11-17 16:12:19 +0000

    Step_2.1

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items, get_item, add_item, save_item
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
 @app.route('/')
-def index(name=None):
-    return render_template("index.html",name=name)
+def index():
+    items = get_items()
+    return render_template("index.html",items=items)
 
 
 if __name__ == '__main__':
