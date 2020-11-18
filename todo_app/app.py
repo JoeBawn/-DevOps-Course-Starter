41f53ce8a205f669cd7e152870c56edde1e799a5
JoeBawn <74307355+JoeBawn@users.noreply.github.com>
2020-11-18 19:10:24 +0000

    Stretch_Goals_Bootstrap.6

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, redirect
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items, get_item, add_item, save_item
-
+from todo_app.data.session_items import get_items, get_item, add_item, save_item, remove_item
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -17,6 +16,24 @@ def add_list():
     newitemtitle = request.form.get('title')
     add_item(newitemtitle)
     return redirect(('/'))
+
+@app.route('/remove_item', methods=['POST'])
+def remove_existing_item():
+    remove_id = request.form.get('remove_id')
+    items = remove_item(remove_id)
+    return redirect(request.headers.get('Referer'))
+
+@app.route('/toggle_status', methods=['POST'])
+def toggle_status():
+    toggle_item = get_item(request.form.get('toggle_item_id'))
+
+    if toggle_item['status'] == "Not Started":
+        toggle_item['status'] = "Completed"
+    else:
+        toggle_item['status'] = "Not Started"
+
+    item = save_item(toggle_item)
+    return redirect(request.headers.get('Referer'))
 
 if __name__ == '__main__':
     app.run()
